63f72568ef6757dfae71357ad9199d8ab694790c
tomyuang <tomyuang@protonmail.com>
2019-06-23 22:54:59 +0800

    代付通知

--- php_sdk/AgentPayNotify.php
+++ php_sdk/AgentPayNotify.php
@@ -9,37 +9,32 @@ include 'PayCommon.class.php';
 $array = array(
     //商户id
     "mchId" => $merchantId,
-    // 商户代付单号
-    "mchOrderNo" => "AP" . round(microtime(true) * 10000),
+    // 代付单号
+    "transOrderId" => $_REQUEST['transOrderId'],
     // 代付金额,单位分
-    "amount" => 119,
-    // 账户属性:0-对私,1-对公,默认对私
-    "accountAttr" => 0,
-    // 收款人账户名
-    "accountName" => "丁伟民",
-    // 收款人账户号
-    "accountNo" => "6222020200443455",
-    // 开户行所在省份(账户属性对公时必填)
-    "province" => "",
-    // 开户行所在市(账户属性对公时必填)
-    "city" => "",
-    // 开户行名称(账户属性对公时必填)
-    "bankName" => "",
-    // 联行号(账户属性对公时必填)
-    "bankNumber" => "",
-    // 回调URL
-    "notifyUrl" => $agentPayNotifyUrl,
-    // 备注
-    "remark" => "代付测试",
-    // 扩展域
-    "extra" => "",
-    // 请求时间
-    "reqTime" => date('YmdHis'),
-    // 用户uid
-    "uid" => 123455555,
-    //用户ip
-    "cip" => "11.23.44.23",
+    "appId" => $_REQUEST['transOrderId'],
+    // 商户订单号
+    "mchOrderNo" => $_REQUEST['mchOrderNo'],
+    // 支付金额
+    "amount" => $_REQUEST['amount'],
+    // 状态
+    "status" => $_REQUEST['status'],
+    // 转账结果
+    "result" => $_REQUEST['result'],
+    // 渠道订单号
+    "channelOrderNo" => $_REQUEST['channelOrderNo'],
+    // 渠道错误描述
+    "channelErrMsg" => $_REQUEST['channelErrMsg'],
+    // 扩展参数1
+    "param1" => $_REQUEST['param1'],
+    // 扩展参数2
+    "param2" => $_REQUEST['param2'],
+    // 转账成功时间
+    "transSuccTime" => $_REQUEST['transSuccTime'],
 );
 
-$sign = PayCommon::createSign($array, $merchantKey);
-$array['sign'] = $sign;
+$result = PayCommon::checkSign($array, $merchantKey, $_REQUEST['sign']);
+
+if($result){
+    echo "success";
+}
